2882277550cfaeac7c018903839e109428dc3158
Dineth De Silva <dinethran33@gmail.com>
2025-10-03 15:32:28 +0530

    chore: add license header to sessionwarningdialog.tsx

--- webapp/src/component/common/SessionWarningDialog.tsx
+++ webapp/src/component/common/SessionWarningDialog.tsx
@@ -1,3 +1,19 @@
+// Copyright (c) 2025 WSO2 LLC. (https://www.wso2.com).
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 import {
   Button,
   Dialog,
@@ -22,10 +38,13 @@ function SessionWarningDialog(props: SessionWarningDialogProps) {
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <DialogTitle id="alert-dialog-title">{"Are you still there?"}</DialogTitle>
+      <DialogTitle id="alert-dialog-title">
+        {"Are you still there?"}
+      </DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          It looks like you've been inactive for a while. Would you like to continue?
+          It looks like you've been inactive for a while. Would you like to
+          continue?
         </DialogContentText>
       </DialogContent>
       <DialogActions>
