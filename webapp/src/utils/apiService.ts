607eba4ff2b941017647ebff429644c0a972f299
dinethde <dinethran33@gmail.com>
2025-10-08 12:29:08 +0530

    chore: remove unnecessary blank line in apiService.ts

--- webapp/src/utils/apiService.ts
+++ webapp/src/utils/apiService.ts
@@ -13,7 +13,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 import * as rax from "retry-axios";
 import axios, { AxiosInstance, CancelTokenSource } from "axios";
 
