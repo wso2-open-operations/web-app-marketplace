8e35233b0282e81bb2979e7a23894fd4cc815de3
dinethde <dinethran33@gmail.com>
2025-10-24 12:09:57 +0530

    feat: add CreateApp, CreateTags, and UpdateApp components

---
+++ webapp/src/view/admin/panel/createApp.tsx
@@ -0,0 +1,7 @@
+export default function CreateApp(){
+    return (
+        <div>
+            <h1>Create App</h1>
+        </div>
+    )
+}
---
+++ webapp/src/view/admin/panel/updateApp.tsx
@@ -0,0 +1,7 @@
+export default function UpdateApp(){
+    return (
+        <div>
+            <h1>Update App</h1>
+        </div>
+    )
+}
