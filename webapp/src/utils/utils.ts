7fca9820291cb3d222466a58e4d2c0533ae7d7dd
dinethde <dinethran33@gmail.com>
2025-12-17 02:34:49 +0530

    migration: fix import statement for CSSProperties in utils.ts

--- webapp/src/utils/utils.ts
+++ webapp/src/utils/utils.ts
@@ -13,7 +13,7 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-import type { CSSProperties } from 'react';
+import { CSSProperties } from "react";
 
 export const isIncludedRole = (a: string[], b: string[]): boolean => {
   return [...getCrossItems(a, b), ...getCrossItems(b, a)].length > 0;
