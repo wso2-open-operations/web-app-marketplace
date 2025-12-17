ef752ece0a00d933f02c107a38076c87770cc849
dinethde <dinethran33@gmail.com>
2025-12-17 14:09:32 +0530

    fix: update SnowflakeIcon to accept color prop instead of using theme

--- webapp/src/assets/icons/SnowFlakeIcon.tsx
+++ webapp/src/assets/icons/SnowFlakeIcon.tsx
@@ -1,14 +1,16 @@
-import { useTheme } from "@mui/material";
+interface SnowflakeIconProps {
+  color: string;
+}
 
-export default function SnowflakeIcon(): HTMLImageElement {
-  const theme = useTheme();
+export default function SnowflakeIcon(props: SnowflakeIconProps): HTMLImageElement {
+  const { color } = props;
   const svg = `<svg
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="${theme.palette.fill.xmas.active}"
+      stroke="${color}"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
