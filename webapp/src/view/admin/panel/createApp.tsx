3a2246de8ecb45fd416b34907bc0765231e2d7ff
dinethde <dinethran33@gmail.com>
2025-12-23 00:47:02 +0530

    feat: move validation schema to a separate file for better organization

--- webapp/src/view/admin/panel/createApp.tsx
+++ webapp/src/view/admin/panel/createApp.tsx
@@ -29,7 +29,6 @@ import {
   Typography,
 } from "@mui/material";
 import { useFormik } from "formik";
-import * as Yup from "yup";
 
 import { useEffect, useState } from "react";
 
@@ -44,6 +43,8 @@ import { RootState, useAppDispatch, useAppSelector } from "@root/src/slices/stor
 import { fetchTags } from "@root/src/slices/tagSlice/tag";
 import { State } from "@root/src/types/types";
 
+import { validationSchema } from "../utils/CreateAppSchema";
+
 const fileSize = 10 * 1024 * 1024;
 
 interface FileWithPreview {
@@ -53,43 +54,6 @@ interface FileWithPreview {
   progress: number;
   error: string | null;
 }
-
-const validationSchema = Yup.object({
-  title: Yup.string()
-    .trim()
-    .min(2, "Title must be at least 2 characters")
-    .required("App name is required"),
-  description: Yup.string()
-    .trim()
-    .min(10, "Description must be at least 10 characters")
-    .max(100, "Description must be at most 100 characters")
-    .required("App description is required"),
-  link: Yup.string().trim().url("Must be a valid URL").required("App URL is required"),
-  versionName: Yup.string()
-    .trim()
-    .min(1, "Version name must be at least 1 character")
-    .required("App version name is required"),
-  tags: Yup.array()
-    .of(Yup.number().required())
-    .min(1, "At least one tag is required")
-    .required("Tags are required"),
-  groupIds: Yup.array()
-    .of(Yup.string().required())
-    .min(1, "At least one user group is required")
-    .required("User groups are required"),
-  icon: Yup.mixed()
-    .required("App icon is required")
-    .test("fileType", "Only SVG files are allowed", (value) => {
-      if (!value) return false;
-      const file = value as File;
-      return file.type === "image/svg+xml" && file.name.toLowerCase().endsWith(".svg");
-    })
-    .test("fileSize", "File size must not exceed 10MB", (value) => {
-      if (!value) return false;
-      const file = value as File;
-      return file.size <= fileSize; // 10MB
-    }),
-});
 
 export default function CreateApp() {
   const dispatch = useAppDispatch();
@@ -224,7 +188,6 @@ export default function CreateApp() {
         flexDirection: "column",
         bgcolor: "background.paper",
         borderRadius: 2,
-        boxShadow: 4,
       }}
     >
       {/* Form Content */}
